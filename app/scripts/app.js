703cfd639911cb26bae75d9479786d107db7c6de
xhanin <xavier.hanin@4sh.fr>
2014-04-30 06:32:44 +0200

    demo activities stream implementation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,4 +15,36 @@ angular.module('rtdm', [
             .otherwise({
                 redirectTo: '/'
             });
-    });
+    })
+    .service('wsClient', function() {
+        var ws = new SockJS('http://rtdm.restx.io/stomp');
+        var client = Stomp.over(ws);
+
+        // SockJS does not support heart-beat: disable heart-beats
+        client.heartbeat.outgoing = 0;
+        client.heartbeat.incoming = 0;
+
+        return client;
+    })
+    .run(function(wsClient, $rootScope) {
+        wsClient.connect('rtdm', '535d500f07b286492e4433af',
+            // on connect
+            function() {
+                var id = wsClient.subscribe("/topic/test", function(d) {
+                    console.log('SockJS Event', d);
+
+                    $rootScope.$apply(function() {
+                        $rootScope.$broadcast('ws', angular.fromJson(d.body));
+                    });
+                });
+            },
+            // on error
+            function(message) {
+                console.log('SockJS Error', message);
+            },
+            '/');
+
+        // test sending event from console
+        // angular.element(document.body).injector().get('wsClient').send('/topic/test', {"content-type":"application/json"}, '{ "event": "CARD_UPDATED"}')
+    })
+;
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('rtdm')
-    .controller('MainCtrl', function ($scope) {
+    .controller('MainCtrl', function ($scope, $http) {
+        $scope.activities = [];
+        $http.get('/api/activities').success(function(data) {
+            $scope.activities = data;
+        });
 
+        $scope.$on('ws', function(event, data) {
+            $scope.activities.push(data);
+        });
     });
